2456fd637d9e76c2361397b37e6000f6de85002c
kipkoech eric <cheruiyotkipkoech612@gmail.com>
2025-02-20 09:56:50 +0000

    message

--- obj/Release/net8.0/linux-x64/GHActions-CiCd.AssemblyInfo.cs
+++ obj/Release/net8.0/linux-x64/GHActions-CiCd.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GHActions-CiCd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b782c9af98b5854441a88e94fd837b3fbc34f8b")]
 [assembly: System.Reflection.AssemblyProductAttribute("GHActions-CiCd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GHActions-CiCd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
